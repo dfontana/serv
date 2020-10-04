5c126a6a0cb48b85f744ee13cac32f5638106036
Dylan Fontana <dyfontana@gmail.com>
2020-10-04 17:58:18 -0400

    fmt and tests

--- streams/src/stream.rs
+++ streams/src/stream.rs
@@ -5,7 +5,7 @@ mod stream_test;
 use std::sync::Arc;
 
 pub struct Stream<T> {
-  data: Vec<Arc<T>>
+  data: Vec<Arc<T>>,
 }
 
 pub fn of<T: Clone>(args: &[T]) -> Stream<T> {
@@ -13,27 +13,36 @@ pub fn of<T: Clone>(args: &[T]) -> Stream<T> {
   for d in args.iter() {
     vec.push(Arc::new(d.clone()))
   }
-  Stream{data: vec}
+  Stream { data: vec }
 }
 
 impl<T> Stream<T> {
-  pub fn map<F, K>(&self, f: F) -> Stream<K> where F: Fn(&T) -> K {
+  pub fn map<F, K>(&self, f: F) -> Stream<K>
+  where
+    F: Fn(&T) -> K,
+  {
     let mut vec = Vec::new();
     for d in self.data.iter() {
       vec.push(Arc::new(f(d)))
     }
-    Stream{data: vec}
+    Stream { data: vec }
   }
-  pub fn filter<F>(&self, f: F) -> Stream<T> where F: Fn(&T) -> bool {
+  pub fn filter<F>(&self, f: F) -> Stream<T>
+  where
+    F: Fn(&T) -> bool,
+  {
     let mut vec = Vec::new();
     for d in self.data.iter() {
       if f(d) {
         vec.push(d.clone());
       }
     }
-    Stream{data: vec}
+    Stream { data: vec }
   }
-  pub fn each<F>(&self, mut f: F) where F: FnMut(&T) {
+  pub fn each<F>(&self, mut f: F)
+  where
+    F: FnMut(&T),
+  {
     for d in self.data.iter() {
       f(d)
     }
--- streams/src/stream_test.rs
+++ streams/src/stream_test.rs
@@ -1,12 +1,45 @@
-use super::of;
+use super::{of, Stream};
+
+// TODO Stream.collect()
+fn collect_stream<T: Copy>(s: Stream<T>) -> Vec<T> {
+  let mut out = Vec::new();
+  s.each(|item| {
+    out.push(*item);
+  });
+  return out;
+}
 
 #[test]
-fn build_and_iter() {
-  let test = of(&["a", "b", "c"]);
+fn has_all_elements() {
   let expected = vec!["a", "b", "c"];
-  let mut actual = Vec::new();
-  test.each(|item| {
-    actual.push(*item);
-  });
-  assert_eq!(actual, expected);
+  let test = of(&["a", "b", "c"]);
+  assert_eq!(collect_stream(test), expected);
+}
+
+#[test]
+fn map_produces_new_stream() {
+  let expected = vec![2, 4, 6];
+  let test = of(&[1, 2, 3]).map(|x| x * 2);
+  assert_eq!(collect_stream(test), expected);
+}
+
+#[test]
+fn filter_omits_items() {
+  let expected = vec![1, 3];
+  let test = of(&[1, 2, 3]).filter(|x| *x != 2);
+  assert_eq!(collect_stream(test), expected);
+}
+
+#[test]
+fn filter_then_map() {
+  let expected = vec![2, 6];
+  let test = of(&[1, 2, 3]).filter(|x| *x != 2).map(|x| x * 2);
+  assert_eq!(collect_stream(test), expected);
+}
+
+#[test]
+fn map_then_filter() {
+  let expected = vec![4, 6];
+  let test = of(&[1, 2, 3]).map(|x| x * 2).filter(|x| *x != 2);
+  assert_eq!(collect_stream(test), expected);
 }
